c7c28f1d65fd67ad6504c3b11e100cfa89fe1cef
IbnRasAl-Fakih <rasul.eletai@gmail.com>
2026-01-14 17:35:58 +0500

    убрал чувствительные данные с application.properties

--- src/main/java/kz/event/EventServerApplication.java
+++ src/main/java/kz/event/EventServerApplication.java
@@ -3,10 +3,19 @@ package kz.event;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import java.io.File;
+
 @SpringBootApplication
 public class EventServerApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .directory(System.getProperty("user.dir"))
+                .ignoreIfMissing()
+                .load();
+        dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+        
         SpringApplication.run(EventServerApplication.class, args);
     }
 }
